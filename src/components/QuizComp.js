c0d96655ffd31f7d98f4ace778f687348b49fe59
Usopp <usopp@danbee.ai>
2022-04-19 11:24:23 +0900

    tts 미완료 기능숨겨서 commit

--- src/components/QuizComp.js
+++ src/components/QuizComp.js
@@ -2,26 +2,28 @@ import React, {useEffect, useRef, useState} from 'react';
 import '../style/QuizComp.css';
 
 import jsonData from "../list";
-console.log(jsonData);
+// console.log(jsonData);
 
 function QuizComp() {
     const quizList = jsonData;
     const MAX_COUNT = 13;
+    // let voices = [];
 
     // useState Hooks
     const [seconds, setSeconds] = useState(0);
     const [showQuiz, setShowQuiz] = useState(false);
+    // const [listenQuestion, setListenQuestion] = useState(false);
 
     const currentIndex = useRef(0);
     const timerId = useRef();
 
     const next = () => {
-        console.log('next!');
+        // console.log('next!');
         stopTimer();
         setSeconds(0);
         // setCurrentIndex(prevState => prevState +1);
         currentIndex.current++;
-        console.log(currentIndex.current);
+        // console.log(currentIndex.current);
         if(currentIndex.current < quizList.length) {
             startTimer();
         } else {
@@ -30,7 +32,7 @@ function QuizComp() {
     }
 
     const startTimer = () => {
-        console.log("start!");
+        // console.log("start!");
         setShowQuiz(true);
         timerId.current = setInterval(() => {
             setSeconds(prevState => prevState +1);
@@ -39,7 +41,7 @@ function QuizComp() {
     }
 
     const stopTimer = () => {
-        console.log('stop!');
+        // console.log('stop!');
         clearInterval(timerId.current);
         timerId.current = 0;
     }
@@ -53,11 +55,69 @@ function QuizComp() {
     }
 
     useEffect(() => {
-        console.log('Seconds: ', seconds);
+        // console.log('Seconds: ', seconds);
         if(seconds >= MAX_COUNT) {
             next();
         }
     }, [seconds])
+
+
+    // tts
+
+    // useEffect(() => {
+    //     voices = window.speechSynthesis.getVoices();
+    //     console.log(voices);
+    // });
+
+    // const toggleListen = () => {
+    //     setListenQuestion(!listenQuestion);
+    // }
+    //
+    // const speech = () => {
+    //     if(!window.speechSynthesis) {
+    //         alert("음성 재생을 지원하지 않는 브라우저입니다. 크롬, 파이어폭스 등의 최신 브라우저를 이용하세요");
+    //         return;
+    //     }
+    //
+    //     voices = window.speechSynthesis.getVoices();
+    //     console.log(voices);
+    //
+    //     let text = quizList[currentIndex.current].question;
+    //     console.log('text:' + text);
+    //     if(!text) {
+    //         console.log("text is empty..");
+    //         return;
+    //     }
+    //
+    //     const lang = 'ko-KR';
+    //     const utterThis = new SpeechSynthesisUtterance(text);
+    //
+    //     utterThis.onend = function (event) {
+    //         console.log('end');
+    //     };
+    //
+    //     utterThis.onerror = function(event) {
+    //         console.log('error', event);
+    //     };
+    //
+    //     var voiceFound = false;
+    //
+    //     for(var i = 0; i < voices.length ; i++) {
+    //         if(voices[i].lang.indexOf(lang) >= 0 || voices[i].lang.indexOf(lang.replace('-', '_')) >= 0) {
+    //             utterThis.voice = voices[i];
+    //             voiceFound = true;
+    //         }
+    //     }
+    //     if(!voiceFound) {
+    //         console.log('voice not found');
+    //         return;
+    //     }
+    //     utterThis.lang = lang;
+    //     utterThis.pitch = 1;
+    //     utterThis.rate = 0.8; //속도
+    //
+    //     window.speechSynthesis.speak(utterThis);
+    // }
 
     return (
         <>
@@ -68,6 +128,7 @@ function QuizComp() {
               </>
             ) : (
                 <>
+                    {/*<button onClick={()=>toggleListen()}>Sound { listenQuestion ? 'On' : 'Off' }</button>*/}
                     { seconds >= 0 && seconds < MAX_COUNT-3 ?
                         (
                             <div className="question--title">
